945736bee2f9ed3c27d34b6258e94c787172fd4d
tothang97nbvn <tothang97nbvn@gmail.com>
2021-04-05 20:14:35 +0700

    thêm text

--- component/card/card.js
+++ component/card/card.js
@@ -8,9 +8,9 @@ export default class Card extends React.Component {
         <div className="card p-3">
           <div className="card-img-top img-responsive img-responsive-16by9" style={{ backgroundImage: "url(./image/developer.png)" }}></div>
           <div className="card-body" style={{ padding: "1rem 0rem" }}>
-            <h3 className="card-title">Card with top image</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti fugit incidunt, iste, itaque minima
-          neque pariatur perferendis sed suscipit velit vitae voluptatem.</p>
+            <h3 className="card-title">Đôi lời giới thiệu</h3>
+            <p>Sudolab là blog chuyên về công nghệ đôi khi có thể có những câu chuyện xàm xí khi đi làm bởi #Tô </p>
+            <p>Sự ủng hộ của bạn giúp mình tích cực viết các bài viết mới hơn ❤</p>
           </div>
         </div>
       </div>
