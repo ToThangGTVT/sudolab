54957bab8d60f4b16670086893bfd3a3bf4630ab
Tô Quang Thắng <33534455+ToThangGTVT@users.noreply.github.com>
2021-04-07 00:37:00 +0700

    Update index.js
    thêm quảng cáo

--- pages/index.js
+++ pages/index.js
@@ -49,6 +49,9 @@ export default class Index extends React.Component {
              src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
           </script>
         </Head>
+        <amp-auto-ads type="adsense"
+           data-ad-client="ca-pub-8590525040934290">
+        </amp-auto-ads>
         <Navbar></Navbar>
         <div className="container pb-5">
           <div className="row">
